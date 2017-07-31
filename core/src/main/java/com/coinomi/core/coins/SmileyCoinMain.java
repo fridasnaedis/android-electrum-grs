d6ba8c5ab93664fd3ecfc24b7d679f64ea2b8c6e
Frida <fridasnaedis@gmail.com>
2017-07-31 14:13:03 +0000

    Add a main SmileyCoin file - may need fix

---
+++ core/src/main/java/com/coinomi/core/coins/SmileyCoinMain.java
@@ -0,0 +1,36 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.BitFamily;;
+
+import org.bitcoinj.core.Coin;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class SmileyCoinMain extends CoinType {
+    private SmileyCoinMain() {
+        id = "smileycoin.main";
+
+        addressHeader = 25;
+        p2shHeader = 5;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+
+        family = BitFamily.get();
+        name = "Smileycoin";
+        symbol = "SMLY";
+        uriScheme = "smileycoin";
+        bip44Index = 17;
+        unitExponent = 8;
+        feePerKb = value(100000000);
+        minNonDust = value(1000); // 0.00001 GRS mininput
+        softDustLimit = value(10000); // 0.00001 GRS
+        softDustPolicy = SoftDustPolicy.BASE_FEE_FOR_EACH_SOFT_DUST_TXO;
+        signedMessageHeader = toBytes("SmileyCoin Signed Message:\n");
+    }
+
+    private static SmileyCoinMain instance = new SmileyCoinMain();
+    public static synchronized SmileyCoinMain get() {
+        return instance;
+    }
+}
